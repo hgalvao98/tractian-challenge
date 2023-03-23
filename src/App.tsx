b1dcb360a7128d8c262556627254c184900c527a
hgalvao98 <henrique.galvao@hotmail.com>
2023-03-23 17:59:18 -0300

    fixed router not working on production

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
-import { HeatMapOutlined } from "@ant-design/icons";
-import { Layout, Menu } from "antd";
-import { useEffect, useState } from "react";
+import { Layout } from "antd";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import "./constants/pages/Home/styles.scss";
 import { dataFetchAll } from "./modules/store/app/actions";
 import RootRoutes from "./routes";
-import { menuItems } from "./utils/menuItems";
 
 export default function App() {
   const dispatch = useDispatch();
-  const [menuItemActive, setMenuItemActive] = useState("");
-  const { Sider } = Layout;
-
-  const handleMenuChange = (e) => {
-    setMenuItemActive(e.key);
-    let url;
-    if (e.key) {
-      url = e?.key?.toLowerCase();
-      window.location.replace(url);
-    } else {
-      window.location.replace("/");
-    }
-  };
 
   useEffect(() => {
     dispatch(dataFetchAll());
@@ -29,22 +14,6 @@ export default function App() {
 
   return (
     <Layout className="home" style={{ height: "100%" }}>
-      <Sider className="sider" breakpoint="lg" collapsedWidth="0">
-        <div
-          onClick={() => handleMenuChange("")}
-          className="home__sider__header"
-        >
-          <HeatMapOutlined />
-          <h3>The Test Company</h3>
-        </div>
-        <Menu
-          onClick={handleMenuChange}
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["0"]}
-          items={menuItems}
-        />
-      </Sider>
       <RootRoutes />
     </Layout>
   );
